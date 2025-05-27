506ebcf2338e6dd0429419a245fd509015804a3a
riolly <alberto.cahyantara@gmail.com>
2025-05-27 15:26:30 +0700

    remove edge

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,10 +1,7 @@
-import type { ServerRuntime } from "next";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 import { appRouter } from "@/server";
 import { createContext } from "@/server/trpc";
-
-export const runtime: ServerRuntime = "edge";
 
 const handler = (req: Request) =>
   fetchRequestHandler({
